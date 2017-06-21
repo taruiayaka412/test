ba4b334f86bfdc94ffabfa273c29c7916378f6b9
tarui.ayaka <tarui.ayaka@tarui>
2017-06-21 09:42:07 +0900

    ファイル

--- github-example/src/githubexample/test/test.java
+++ github-example/src/githubexample/test/test.java
@@ -3,7 +3,7 @@ package githubexample.test;
 public class test {
 
 	    public static void main(String[] args) {
-	        System.out.println("Hello, Git!");
+	        System.out.println("Hello, Git");
 	    }
 	}
 
